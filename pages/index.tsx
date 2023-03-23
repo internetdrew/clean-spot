1fcc01a8e4607836d6cba38a935bcbca9be3b1f8
internetdrew <andrew@internetdrew.com>
2023-03-23 13:05:22 -0400

    install recoil and login button bounce

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 export default function Home() {
+  const router = useRouter();
+
+  console.log(router);
+
   return (
     <>
       <h1>hello world</h1>
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,3 @@
-import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
 import { useState, useEffect } from 'react';
 
 const login = () => {
@@ -28,14 +27,15 @@ const login = () => {
   return (
     <>
       <section className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%]'>
-        <div className='flex flex-col gap-20'>
+        <div className='flex flex-col gap-20 sm:gap-10'>
           <div className='text-center'>
             <h1 className='text-3xl'>Clean Spot</h1>
             <p className='text-xl'>
-              Clean your favorite explicit Spotify playlists with ease.
+              Create family-friendly versions of your favorite explicit Spotify
+              playlists with ease.
             </p>
           </div>
-          <div className='text-center'>
+          <div className='text-center motion-safe:animate-bounce'>
             <button className='p-2 border-solid border-2 border-black text-lg shadow-[2px_2px_2px_1px_rgb(0,0,0,1)] active:scale-90 transition-all ease-in-out duration-300'>
               <a href={authUrl}>Login with Spotify</a>
             </button>
