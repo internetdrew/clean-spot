9da96bc87fc263e0a2690c8af1f33a2e42c7bf82
internetdrew <andrew@internetdrew.com>
2023-03-24 08:31:36 -0400

    kick user to login without proper credentials

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,10 +3,12 @@ import { v4 } from 'uuid';
 export default function handler(req, res) {
   const authState = v4();
   const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-  const redirectUrl = `https://${req.headers.host}`;
-  // window.localStorage.setItem('authState', JSON.stringify(authState));
+  const redirectUrl = `http://${req.headers.host}`;
+  console.log(req);
+  const reqStr = JSON.stringify(req.query);
   res.status(200).json({
     url: `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&response_type=token&state=${authState}`,
     authState,
+    reqStr,
   });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { tokenState } from '@/atoms/tokenAtom';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [user, setUser] = useState({});
-  // console.log(user);
   const [token, setToken] = useRecoilState(tokenState);
+  const router = useRouter();
 
   useEffect(() => {
     const params = window.location.hash.split('&');
-    const accessToken = params[0].split('=')[1];
+    const accessToken = params[0]?.split('=')[1];
+    const returnedStateVar = params[3]?.split('=')[1];
+    const localStateVar = localStorage.getItem('authState');
+    if (!returnedStateVar || !params || returnedStateVar !== localStateVar)
+      router.push('/login');
     setToken(accessToken);
     try {
       fetch('https://api.spotify.com/v1/me', {
