1e7115969f4ab0e1201a137ac7a2c7ce36b38cf9
internetdrew <andrew@internetdrew.com>
2023-03-26 21:16:04 -0400

    implement next link

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const login = ({ authUrl, protocol }: any) => {
   return (
     <>
@@ -12,7 +14,8 @@ const login = ({ authUrl, protocol }: any) => {
           </div>
           <div className='text-center motion-safe:animate-bounce'>
             <button className='p-2 border-solid border-2 border-black text-lg shadow-[2px_2px_2px_1px_rgb(0,0,0,1)] active:scale-90 transition-all ease-in-out duration-300'>
-              <a href={`${protocol}${authUrl}`}>Login with Spotify</a>
+              {/* <a href={`${protocol}${authUrl}`}>Login with Spotify</a> */}
+              <Link href={`${protocol}${authUrl}`}>Login with Spotify</Link>
             </button>
           </div>
         </div>
@@ -23,17 +26,17 @@ const login = ({ authUrl, protocol }: any) => {
 
 export default login;
 
-// export const getServerSideProps = async ({ req, res }: any) => {
-//   const baseUrl = req.headers.referer.split('/login')[0];
-//   console.log(baseUrl);
-//   const protocol = req.headers.referer.split(':')[0] + '://';
-//   console.log(protocol);
-//   const response = await fetch(`${baseUrl}/api/login`, {
-//     method: 'POST',
-//     body: baseUrl,
-//   });
-//   console.log(response);
-//   const { authUrl } = await response.json();
-//   console.log(authUrl);
-//   return { props: { authUrl, protocol } };
-// };
+export const getServerSideProps = async ({ req, res }: any) => {
+  const baseUrl = req.headers.referer.split('/login')[0];
+  console.log(baseUrl);
+  const protocol = req.headers.referer.split(':')[0] + '://';
+  console.log(protocol);
+  const response = await fetch(`${baseUrl}/api/login`, {
+    method: 'POST',
+    body: baseUrl,
+  });
+  console.log(response);
+  const { authUrl } = await response.json();
+  console.log(authUrl);
+  return { props: { authUrl, protocol } };
+};
